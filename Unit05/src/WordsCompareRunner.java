069cd7d1946b11f9ccfbadd5bd7ccde666cc4905
ybKenobi <yajnvb@gmail.com>
2020-02-10 20:14:25 -0800

    Finished WordsCompare

--- Unit05/src/WordsCompareRunner.java
+++ Unit05/src/WordsCompareRunner.java
@@ -12,7 +12,32 @@ public class WordsCompareRunner
 {
    public static void main( String args[] )
    {
+	   //Instantiation
+	   WordsCompare test = new WordsCompare();
+	   
 		//add test cases
+	   test.setWords("Hello", "Cat");
+	   test.compare();
+	   out.println(test);
 
+	   test.setWords("Green", "Blue");
+	   test.compare();
+	   out.println(test);
+	   
+	   test.setWords("123", "124");
+	   test.compare();
+	   out.println(test);
+	   
+	   test.setWords("ZXY", "YOU");
+	   test.compare();
+	   out.println(test);
+	   
+	   test.setWords("And", "Ant");
+	   test.compare();
+	   out.println(test);
+	   
+	   test.setWords("CSA", "PHYSICS");
+	   test.compare();
+	   out.println(test);
 	}
 }
