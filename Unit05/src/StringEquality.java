c4aa93ea4c76916ff57183d5cfd6e4cd8a65be07
ybKenobi <yajnvb@gmail.com>
2020-02-10 19:57:04 -0800

    Finished StringEquality

--- Unit05/src/StringEquality.java
+++ Unit05/src/StringEquality.java
@@ -10,27 +10,40 @@ import static java.lang.System.*;
 
 public class StringEquality
 {
-	private String wordOne, wordTwo;
+	private String wordOne, wordTwo, result;
 
 	public StringEquality()
 	{
+		setWords("","");
 	}
 
 	public StringEquality(String one, String two)
 	{
+		setWords(one, two);
 	}
 
 	public void setWords(String one, String two)
 	{
+		wordOne = one;
+		wordTwo = two;
 	}
 
-	public boolean checkEquality( )
+	public String checkEquality( )
 	{
-		return false;
+		if (wordOne.equals(wordTwo))
+		{
+			result = "";
+			return result;
+		}
+		else
+		{
+			result = "not ";
+			return result;
+		}
 	}
 
 	public String toString()
 	{
-		return wordOne + " does not have the same letters as " + wordTwo + "\n";
+		return wordOne + " does " + result + "have the same letters as " + wordTwo + "\n";
 	}
 }
--- Unit05/src/StringEqualityRunner.java
+++ Unit05/src/StringEqualityRunner.java
@@ -12,7 +12,36 @@ public class StringEqualityRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
+		StringEquality test = new StringEquality();
 		
+		//test1
+		test.setWords("abs", "abc");
+		test.checkEquality();
+		System.out.println(test);
+		
+		//test2
+		test.setWords("Cats", "Dogs");
+		test.checkEquality();
+		System.out.println(test);
+		
+		//test3
+		test.setWords("Green", "Green");
+		test.checkEquality();
+		System.out.println(test);
+		
+		//test4
+		test.setWords("Popcorn", "Pop");
+		test.checkEquality();
+		System.out.println(test);
+		
+		//test5
+		test.setWords("1235", "5321");
+		test.checkEquality();
+		System.out.println(test);
+		
+		//test6
+		test.setWords("1234", "1234");
+		test.checkEquality();
+		System.out.println(test);
 	}
 }
