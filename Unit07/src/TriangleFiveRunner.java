467a50f6cc59e7ed5945c6bf86400ed5244a61cd
ybKenobi <yajnvb@gmail.com>
2020-02-16 23:23:33 -0800

    Added more test cases to TriangeFiveRunner

--- Unit07/src/TriangleFiveRunner.java
+++ Unit07/src/TriangleFiveRunner.java
@@ -8,7 +8,23 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {	   
-	   TriangleFive test = new TriangleFive('X', 9);
+	   TriangleFive test = new TriangleFive('C', 4);
+	   System.out.println(test);
+	   
+	   test.setLetter('A');
+	   test.setAmount(5);
+	   System.out.println(test);
+	   
+	   test.setLetter('B');
+	   test.setAmount(7);
+	   System.out.println(test);
+	   
+	   test.setLetter('X');
+	   test.setAmount(6);
+	   System.out.println(test);
+	   
+	   test.setLetter('Z');
+	   test.setAmount(8);
 	   System.out.println(test);
 	}
 }
