dd5d9dc35128578be5dd0468d92df3f4fcf6c8de
ybKenobi <yajnvb@gmail.com>
2020-03-08 16:48:50 -0700

    Finished Doggies activity

--- Unit11/src/Doggies.java
+++ Unit11/src/Doggies.java
@@ -12,22 +12,52 @@ public class Doggies
 	{
 		//point pups at a new arry of Dog
 		
+		pups = new Dog[size];
+		
+		
 	}
 	
 	public void set(int spot, int age, String name)
 	{
+		if (spot >= 0 && spot < pups.length)
+		{
+			Dog temp = new Dog(age, name);
+			pups[spot] = temp;
+		}
+		
+		
 		//put a new Dog in the array at spot 
 		//make sure spot is in bounds		
 	}
 
 	public String getNameOfOldest()
 	{
-		return null;
+		int oldest = pups[0].getAge();
+		int index = 0;
+		for (int i = 1; i < pups.length; i++)
+		{
+			if (oldest < pups[i].getAge())
+			{
+				oldest = pups[i].getAge();
+				index = i;
+			}
+		}
+		return pups[index].getName();
 	}
 
 	public String getNameOfYoungest()
 	{
-		return null;
+		int youngest = pups[0].getAge();
+		int index = 0;
+		for (int i = 1; i < pups.length; i++)
+		{
+			if (youngest > pups[i].getAge())
+			{
+				youngest = pups[i].getAge();
+				index = i;
+			}
+		}
+		return pups[index].getName();
 	}
 
 	public String toString()
